404c92c6a3b5d03a0204d2589c989cc64ea40b1c
snow <emilythach005@gmail.com>
2026-01-23 13:22:26 -0800

    Fixed cponfig

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-<<<<<<< HEAD
-    // ⚠️ Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
-  },
-=======
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
->>>>>>> ee60d66 (vercel deployment)
 }
 
 module.exports = nextConfig
